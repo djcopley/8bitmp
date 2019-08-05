c52379d04cb3981ab7c1ca174044ecc9f26557c8
Daniel Copley <djcopley@users.noreply.github.com>
2019-08-04 17:17:56 -0700

    init hdl

---
+++ hdl/cpu.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library cpu_lib;
+use cpu_lib.helpers.all;
+
+entity cpu is
+  port(
+    clk : in std_logic;
+    rst : in std_logic
+  );
+end entity cpu;
+
+architecture rtl of cpu is
+
+begin
+
+end architecture rtl;
+
